31bcc11a80f40511fe802fe118877b44769c4a7e
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2023-11-22 14:54:02 +0100

    Previous entires

--- subwayTracker/script.js
+++ subwayTracker/script.js
@@ -1,11 +1,18 @@
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 
 let count = 0;
 
-function increment (){
-    count = count + 1
+function increment(){
+    count += 1;
     countEl.innerHTML = count;
-    console.log(count);
+}
+
+function save(){
+   let saveCount = count + " - ";
+   saveEl.innerHTML += saveCount;
+   countEl.innerHTML = 0;
+   count = 0; 
 }
 
 
