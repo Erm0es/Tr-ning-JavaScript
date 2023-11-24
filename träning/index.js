6052bde36d4441fa1d56c2c276693fdee0568ce6
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2023-11-24 10:43:52 +0100

    for loop training

--- träning/index.js
+++ träning/index.js
@@ -1,23 +1,5 @@
+let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
 
-let randomAge = Math.floor(Math.random()* 100 + 1);
-
-
-let age = randomAge;
-let ticketprize = "";
-
-if(age < 6 ){
-    ticketprize = "free";
-}else if(age > 6 && age <= 17){
-    ticketprize = "Child discount"
-}else if(age >= 18 && age <= 26){
-    ticketprize = "Student discount"
-}else if (age >= 27 && age <= 66){
-    ticketprize = "Full price"
-}else if (age >= 66){
-    ticketprize = "Senior discount"
-} else{
-    console.error("Try again")
+for(let i = 0; i < largeCountries.length; i++){
+    console.log(largeCountries[i])
 }
-
-console.log(randomAge);
-console.log(ticketprize);
