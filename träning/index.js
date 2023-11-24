def144659190cdd57bed37afe07a3050541156c2
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2023-11-24 10:37:17 +0100

    if and mathrandom training

--- träning/index.js
+++ träning/index.js
@@ -1,11 +1,23 @@
-let person = {
-    name: "Emelie",
-    age: 28,
-    country: "Sweden"
+
+let randomAge = Math.floor(Math.random()* 100 + 1);
+
+
+let age = randomAge;
+let ticketprize = "";
+
+if(age < 6 ){
+    ticketprize = "free";
+}else if(age > 6 && age <= 17){
+    ticketprize = "Child discount"
+}else if(age >= 18 && age <= 26){
+    ticketprize = "Student discount"
+}else if (age >= 27 && age <= 66){
+    ticketprize = "Full price"
+}else if (age >= 66){
+    ticketprize = "Senior discount"
+} else{
+    console.error("Try again")
 }
 
-function logData(){
-   console.log(person.name + " is " + person.age + " years old and lives in " + person.country);
-}
-
-logData()
+console.log(randomAge);
+console.log(ticketprize);
