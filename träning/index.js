5aef9c95918c651a562cbfc7f1a02a1bd7e3b62b
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2023-12-04 10:54:20 +0100

    template strings training

--- träning/index.js
+++ träning/index.js
@@ -5,3 +5,15 @@ container.innerHTML += "<button onclick='buy()'>Buy!</button>";
 function buy(){
     container.innerHTML += "<p>Thank you for buying</p>"
 };
+
+
+const recipient = "James"
+const sender = "Emelie"
+
+//const email = "Hey " + recipient + "! How is it going? Cheers Per"
+const email = `
+"Hey "${recipient }"! 
+How is it going? 
+Cheers ${sender}"`
+
+console.log(email)
