31fa7e7c8bafd8d6fc1a03c7fd5d94afa7de767c
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2023-12-04 10:44:19 +0100

    making myleads targeted

--- ChromeExtension/script.js
+++ ChromeExtension/script.js
@@ -6,18 +6,18 @@ const ulEl = document.getElementById("ul-el");
 let myLeads = [];
 
 
-inputBtn.addEventListener("click", function(){
+inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
     inputEl.value = "";
     renderLeads();
 });
 
 
-function renderLeads(){
-let listItems = "";
-for(let i = 0; i < myLeads.length; i++){
-    listItems += "<i>"+ myLeads[i]+"</i>"
-}
+function renderLeads() {
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    }
 
-ulEl.innerHTML = listItems;
+    ulEl.innerHTML = listItems;
 }
